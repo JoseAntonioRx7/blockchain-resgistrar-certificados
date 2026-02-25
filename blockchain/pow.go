588dfdddf1f8f333aef575b0b83d0932de16606a
josecrypto <ramosjoseantonio254@gmail.com>
2026-02-25 00:18:43 -0300

    add-lucas-file

--- blockchain/pow.go
+++ blockchain/pow.go
@@ -1,14 +1,21 @@
 package blockchain
 
-import "bytes"
+import (
+	"bytes"
+)
 
-const Difficulty = 3
+// Subimos para 4 para aumentar a segurança da startup
+const Difficulty = 4 
 
 func (b *Block) Mine() {
 	var hash []byte
+    
+    // Criamos o prefixo de zeros dinamicamente com base na Difficulty
+    target := bytes.Repeat([]byte{0}, Difficulty)
+
 	for {
 		hash = b.calculateHash()
-		if bytes.HasPrefix(hash, []byte{0, 0, 0}) {
+		if bytes.HasPrefix(hash, target) {
 			break
 		}
 		b.Nonce++
